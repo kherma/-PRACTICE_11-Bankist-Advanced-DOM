b57ae829a8307f122f8090e1869c8d94e9778817
kherma <kamil.herma01@gmail.com>
2020-11-24 20:46:39 +0100

    Implement operations slider

--- script.js
+++ script.js
@@ -51,3 +51,30 @@ document
       document.querySelector(idFromHref).scrollIntoView({ behavior: 'smooth' });
     }
   });
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove activ from button
+  tabs.forEach(btn => btn.classList.remove('operations__tab--active'));
+
+  // Active clicked button
+  clicked.classList.add('operations__tab--active');
+
+  // Remove active from content
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  // Add active to choosen content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
